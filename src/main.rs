da817bb2079c80759e853321d3f4f12245f0202b
A.J. Ruckman <aj@ruckman.dev>
2020-12-03 17:11:13 -0500

    Day 2

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use std::io;
 use std::fs::File;
-use std::io::{BufReader, BufRead};
+use std::io::{BufReader, BufRead, Error, ErrorKind};
 use itertools::Itertools;
+use regex::Regex;
 
 fn main() -> io::Result<()> {
-    println!("Hello, world!");
+    day2_p2().unwrap();
 
+    return Ok(());
+}
+
+fn day1() -> io::Result<u32> {
     let file = File::open("input/day1.txt")?;
     let reader = BufReader::new(file);
 
@@ -35,9 +40,96 @@ fn main() -> io::Result<()> {
 
         if left + mid + right == 2020 {
             println!("{} {} {} -> {}", left, mid, right, left * mid * right);
-            break;
+
+            return Ok(left * mid * right);
         }
     }
 
-    Ok(())
+    return Err(Error::new(ErrorKind::NotFound, "No results"))
+    // Ok(())
+}
+
+fn day2_p1() -> io::Result<u16> {
+    let file = File::open("input/day2.txt")?;
+    let reader = BufReader::new(file);
+    let re = Regex::new(r"^(\d+)-(\d+) ([a-z]): (\w+)$").unwrap();
+    let mut last: String;
+    let mut valid: u16 = 0;
+
+    for line in reader.lines() {
+        last = line.unwrap();
+
+        let cap = re.captures(&last).unwrap();
+
+        let min = cap[1].parse::<u8>().unwrap();
+        let max = cap[2].parse::<u8>().unwrap();
+        let target = cap[3].parse::<char>().unwrap();
+        let password = &cap[4];
+
+        //
+
+        let mut count: u8 = 0;
+
+        for v in password.chars() {
+            if v == target {
+                count += 1;
+            }
+        }
+
+        if min <= count && count <= max {
+            println!("{}", password);
+            valid += 1;
+        }
+    }
+
+    println!("Valid passwords: {}", valid);
+
+    //
+
+    Ok(valid)
+}
+
+fn day2_p2() -> io::Result<u16> {
+    let file = File::open("input/day2.txt")?;
+    let reader = BufReader::new(file);
+    let re = Regex::new(r"^(\d+)-(\d+) ([a-z]): (\w+)$").unwrap();
+    let mut last: String;
+    let mut valid: u16 = 0;
+
+    for line in reader.lines() {
+        last = line.unwrap();
+
+        let cap = re.captures(&last).unwrap();
+
+        let pos1 = cap[1].parse::<u8>().unwrap();
+        let pos2 = cap[2].parse::<u8>().unwrap();
+        let target = cap[3].parse::<char>().unwrap();
+        let password = &cap[4];
+
+        //
+
+        let mut count: u8 = 0;
+
+        if password.len() < pos2 as usize {
+            continue;
+        }
+
+        if password.as_bytes()[(pos1 - 1) as usize] as char == target {
+            count += 1;
+        }
+
+        if password.as_bytes()[(pos2 - 1) as usize] as char == target {
+            count += 1;
+        }
+
+        if count == 1 {
+            valid += 1;
+        }
+    }
+
+    println!("Valid passwords: {}", valid);
+
+    //
+
+    Ok(valid)
 }
